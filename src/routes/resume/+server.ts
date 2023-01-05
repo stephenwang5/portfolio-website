4c0025da942d1c103667dfa9ab4df394549877d5
Stephen Wang <ste53539@gmail.com>
2023-01-05 17:25:59 -0500

    changed resume file name

--- src/routes/resume/+server.ts
+++ src/routes/resume/+server.ts
@@ -3,5 +3,5 @@ import { redirect } from "@sveltejs/kit";
 export const prerender = true;
 
 export function GET() {
-  throw redirect(302, "resume.pdf");
+  throw redirect(302, "stephen-xiren-wang-resume.pdf");
 }
